26e592386674f053ccb21d437163d6b8bb0aa4e7
kennetholvr <kennetholvr@gmail.com>
2018-09-07 11:29:36 -0500

    set up form

--- client/components/link_form.js
+++ client/components/link_form.js
@@ -1,4 +1,9 @@
 angular.module('app')
   .component('linkForm',{
     templateUrl: '/templates/link_form.html',
+    controller: function(){
+      this.onSubmit = (url, description)=>{
+        console.log({url, description})
+      }
+    }
   })
