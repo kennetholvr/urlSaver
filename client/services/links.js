1e346fd934f0ec6fedd717820c870f5ad720aa6a
kennetholvr <kennetholvr@gmail.com>
2018-09-07 13:54:57 -0500

    connected the server

--- client/services/links.js
+++ client/services/links.js
@@ -1,6 +1,6 @@
 angular.module('app').service('linkService', function linkService($http) {
   this.send = ({url, description}) =>{
-    $http.post('/links')
+    $http.post('/links', { url, description })
     .then((response) => {
       console.log(response)
       }).catch((err) => {
---
+++ server/server.js
@@ -0,0 +1,27 @@
+//create and export express 
+const morgan = require('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+const logger = ({method, path, body}, res, next) =>{
+  console.log(`*** ${method} to ${path} ***`)
+  if(body) {console.log(`*** ${JSON.stringify(body)} ***`)}
+  next();
+}
+
+app.use(logger);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(morgan('combined'))
+app.use(express.static('client'));
+
+app.post('/links', (req, res)=>{
+  console.log(req.body)
+  res.send(201);
+})
+
+//export the app
+module.exports = app;
